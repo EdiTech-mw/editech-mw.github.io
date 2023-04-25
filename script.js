7ac4270fcfe658b4d862f34d51258d0c886106ca
Chasyxx <105890603+Chasyxx@users.noreply.github.com>
2023-04-24 21:40:22 -0600

    more changes
    split menus!

--- script.js
+++ script.js
@@ -3,31 +3,39 @@ import "./jquery.js";
 const splashes = [
 	"[object Object]",
 	"[object JSON]",
-	"<i><b>DO YOU LIKE MY JQUERY ANIMATIONS???</b></i>"
-];
+	"<i><b>DO YOU LIKE MY JQUERY ANIMATIONS???</b></i>",
+	"Note to juan: Pale moon is ancient and doesn't like new stuff!",
+	`DIE! <i>throws die...</i> a ${Math.round(Math.random() * 6)}!`,
+]
 
 let x1 = document.getElementById('hi1');
 let x2 = document.getElementById('hi2');
 let info = document.getElementsByClassName('info')
 
-x2.innerHTML = splashes[Math.floor(Math.random()*splashes.length)]
+await new Promise(resolve => { $(document).ready(resolve) }) // Wait for jQuery to load
 
-$(document).ready(function () {
-	$(x1).animate({ opacity: '1' }, 1500, function () {
-		$(x1).removeClass('hidden');
-	});
-	setTimeout(()=>{
-		$(x2).animate({ opacity: '1' }, 1000, function () {
-			$(x2).removeClass('hidden');
-		});
-	},500)
-	setTimeout(async ()=>{
-		for (let i = 0; i < info.length; i++) {
-			let x3 = info.item(i)
-			$(x3).animate({ opacity: '1' }, 500, function () {
-				$(x3).removeClass('hidden');
-			});
-			await new Promise(resolve => (setTimeout(resolve,250)))
-		}
-	},1000)
+x2.onclick = async () => {
+	await new Promise(resolve => { $(x2).animate({ opacity: '0' }, 250, "swing", resolve) })
+	x2.innerHTML = splashes[Math.round(Math.random() * (splashes.length - 1))]
+	$(x2).animate({ opacity: '1' }, 250, "swing")
+}
+
+x2.innerHTML = splashes[Math.round(Math.random() * (splashes.length - 1))]
+
+$(x1).animate({ opacity: '1' }, 1500, function () {
+	$(x1).removeClass('hidden');
 });
+setTimeout(() => {
+	$(x2).animate({ opacity: '1' }, 1000, function () {
+		$(x2).removeClass('hidden');
+	});
+}, 500)
+setTimeout(async () => {
+	for (let i = 0; i < info.length; i++) {
+		let x3 = info.item(i)
+		$(x3).animate({ opacity: '1' }, 500, function () {
+			$(x3).removeClass('hidden');
+		});
+		await new Promise(resolve => (setTimeout(resolve, 250)))
+	}
+}, 1000)
